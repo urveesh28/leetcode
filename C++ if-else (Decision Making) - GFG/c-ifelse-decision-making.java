2e95619a3648303143a052be3197cf581a02fa9e
Urveesh-28 <113619189+Urveesh-28@users.noreply.github.com>
2023-06-15 22:02:18 +0530

    Added solution - LeetHub

---
+++ C++ if-else (Decision Making) - GFG/c-ifelse-decision-making.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.compareFive(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String compareFive(int N){
+        // code here
+        if(N>5)return("Greater than 5");
+        else if(N<5)return("Less than 5");
+        return("Equal to 5");
+    }
+}
