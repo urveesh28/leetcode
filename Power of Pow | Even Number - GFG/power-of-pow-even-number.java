4e3cb62407e54c3d55035e42ad36c2c32a62557c
Urveesh Mathur <113619189+Urveesh-28@users.noreply.github.com>
2023-07-16 13:12:50 +0530

    Added solution - LeetHub

---
+++ Power of Pow | Even Number - GFG/power-of-pow-even-number.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.sum_of_square_evenNumbers(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long sum_of_square_evenNumbers(long n)
+    {
+        // Code here
+        long sum=0;
+        int val=2;
+        for(int i=1;i<=n;i++){
+            sum=sum+(val*val);
+            val+=2;
+        }
+        return sum;
+    }
+}
