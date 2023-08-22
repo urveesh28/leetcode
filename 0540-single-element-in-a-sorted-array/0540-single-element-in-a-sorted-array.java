cc29a4c03bad2a815182e3791a4287dbe94eef52
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2023-08-22 21:49:16 +0530

    Time: 1 ms (25.96%), Space: 49.6 MB (88.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,7 +1,22 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int val=-1;
+        boolean[] arr=new boolean[nums[nums.length-1]+1];
+        //applying linear search
+        int ans=-1;
+        for(int i=0;i<nums.length;i++){
+            int val=nums[i];
+            if(arr[val]==true){
+                arr[val]=false;
+            }
+            else arr[val]=true;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==true){
+                ans=i;
+            }
+        }
+        /*int val=-1;
         for(int i : nums){
             if(set.contains(i))set.remove(i);
             else set.add(i);
@@ -9,6 +24,7 @@ class Solution {
         for(int i : set){
             val=i;
         }
-        return val;
+        return val;*/
+        return ans;
     }
 }
