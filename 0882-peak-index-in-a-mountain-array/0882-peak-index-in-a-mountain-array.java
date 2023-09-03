b4cbf3b98356a55dbc2643548662f0abd67aef2d
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2023-09-03 13:56:59 +0530

    Time: 1 ms (22.54%), Space: 57.4 MB (15.81%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -2,10 +2,10 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         //using simple linear search
         int maxindex=-1;
-        int maxval=Integer.MIN_VALUE;
+        //int maxval=Integer.MIN_VALUE;
         for(int i=0;i<arr.length-1;i++){
             if(arr[i]>arr[i+1]){
-                maxval=Math.max(arr[i],maxval);
+                //maxval=Math.max(arr[i],maxval);
                 maxindex=i;
                 break;
             }
