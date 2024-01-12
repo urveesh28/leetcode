ce0c65050c623f33cf0d64ff736b5e9804355e66
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-01-12 23:31:21 +0530

    Time: 3 ms (88.24%), Space: 41.4 MB (43.26%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1704-determine-if-string-halves-are-alike.java
@@ -4,15 +4,31 @@ class Solution {
         int count=0;
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')count++;
+            
         }
         return count;
     }
     public boolean halvesAreAlike(String s) {
+        /*
         String a=s.substring(0,s.length()/2);
         String b=s.substring(s.length()/2,s.length());
         int count1=count(a);
         int count2=count(b);
+        */
+
+        //better approach
+        int count1=0;
+        int count2=0;
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            char c1=s.charAt(i);
+            char c2=s.charAt(j);
+            if(c1=='a'||c1=='A'||c1=='e'||c1=='E'||c1=='i'||c1=='I'||c1=='o'||c1=='O'||c1=='u'||c1=='U')count1++;
+            if(c2=='a'||c2=='A'||c2=='e'||c2=='E'||c2=='i'||c2=='I'||c2=='o'||c2=='O'||c2=='u'||c2=='U')count2++;
+            i++;
+            j--;
+        }        
         if(count1==count2)return true;
         return false;
     }
