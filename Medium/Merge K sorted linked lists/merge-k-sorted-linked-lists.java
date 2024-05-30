abd9e4a29f31c82f8da3df974b814fe6c4441696
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-05-30 21:17:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    
+    public static Node helper(PriorityQueue<Integer> pq){
+        Node head=null , tail=null;
+        while(!pq.isEmpty()){
+            Node t=new Node(pq.remove());
+            if(head==null){
+                head=tail=t;
+            }
+            else{
+                tail.next=t;
+                tail=t;
+            }
+        }
+        return head;
+    }
+    
+    Node mergeKList(Node[]arr,int K)
+    {
+        //Add your code here.
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
+            return (a-b);
+        });
+        
+        for(int i=0;i<K;i++){
+            Node t=arr[i];
+            while(t!=null){
+                pq.add(t.data);
+                t=t.next;
+            }
+        }
+        
+        return helper(pq);
+        
+        
+    }
+}
