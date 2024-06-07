b0d3beb4c36f7a020c38d296c0f8ea61f58ca563
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-06-08 00:52:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Index of an Extra Element/index-of-an-extra-element.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class ExtraElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n - 1];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            for (int i = 0; i < n - 1; i++) b[i] = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.findExtra(n, a, b));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class Solution {
+    public int findExtra(int n, int arr1[], int arr2[]) {
+        // add code here.
+        int l=0;
+        int e=n-2;
+        
+        //left traversing
+        while(l<=e){
+            int m=l+(e-l)/2;
+            if(arr1[m]==arr2[m]){
+                l=m+1;
+            }
+            else{
+                e=m-1;
+            }
+        }
+        
+        return l;
+    }
+}
