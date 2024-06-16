3c091e4644182e797d557bac38fca6242a0118f9
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-06-16 22:40:39 +0530

    Time: 6 ms (27.74%), Space: 42.5 MB (6.67%) - LeetHub

---
+++ 1449-print-words-vertically/1449-print-words-vertically.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> printVertically(String s) {
+        List<String> list=new ArrayList<>();
+        String[] arr=s.split(" ");
+        System.out.println(Arrays.toString(arr));
+
+        //how
+        //are
+        //you
+        int no_of_words=0;
+        for(String str : arr){
+            no_of_words=Math.max(no_of_words,str.length());
+        }
+
+        for(int i=0;i<no_of_words;i++){
+            String ans="";
+            for(int j=0;j<arr.length;j++){
+                if(i>=arr[j].length()){
+                    ans+=" ";
+                }
+                else{
+                    ans+=arr[j].charAt(i);
+                }
+            }
+            list.add(ans);
+        }
+
+        for(int i=0;i<list.size();i++){
+            String str=list.get(i).stripTrailing();
+            list.set(i,str);
+        }
+
+        return list;
+    }
+}
