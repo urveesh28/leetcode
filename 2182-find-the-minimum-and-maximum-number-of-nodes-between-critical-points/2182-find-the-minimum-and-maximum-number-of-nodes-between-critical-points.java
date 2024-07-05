bf3cca455af6fca4a54c785467d3f6d70c361ab7
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-07-05 18:50:34 +0530

    Time: 9 ms (39.56%), Space: 69.2 MB (11.72%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -11,7 +11,7 @@
 
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        List<int[]> arr=new ArrayList<>();
+        List<Integer> arr=new ArrayList<>();
         ListNode temp1=head;
         ListNode temp2=head.next;
         ListNode temp3=head.next.next;
@@ -20,14 +20,10 @@ class Solution {
         while(temp3!=null){
             
             //local maxima/minima
-            int[] a=new int[2];
-            a[0]=temp2.val;
-            a[1]=pos+1;
-
+            
             if((temp1.val<temp2.val && temp2.val>temp3.val)||(temp1.val>temp2.val && temp2.val<temp3.val)){  
-                arr.add(a);
+                arr.add(pos+1);
             }
-
 
             temp1=temp1.next;
             temp2=temp2.next;
@@ -35,18 +31,14 @@ class Solution {
             pos++;
         }
         
-        for(int i=0;i<arr.size();i++){
-            System.out.println(arr.get(i)[0]+" "+arr.get(i)[1]);
-        }
-
         if(arr.size()<2)return new int[]{-1,-1};
         
         int min=Integer.MAX_VALUE;
         for(int i=1;i<arr.size();i++){
-            min=Math.min(min,arr.get(i)[1]-arr.get(i-1)[1]);
+            min=Math.min(min,arr.get(i)-arr.get(i-1));
         }
 
-        return new int[]{min,arr.get(arr.size()-1)[1]-arr.get(0)[1]};
+        return new int[]{min,arr.get(arr.size()-1)-arr.get(0)};
 
     }
 }
