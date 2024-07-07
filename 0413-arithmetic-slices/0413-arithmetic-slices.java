48b43f50f776fc38dbe999c34ee1074673d2ec11
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-07-07 16:35:38 +0530

    Time: 0 ms (100%), Space: 41.4 MB (29.05%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -5,7 +5,7 @@ class Solution {
         int a=0;
         
         for(int i=0;i<nums.length-2;i++){
-            if((2*nums[i+1])==(nums[i]+nums[i+2])){
+            if((nums[i+1]-nums[i])==(nums[i+2]-nums[i+1])){
                 a++;
                 c+=a;
             }
