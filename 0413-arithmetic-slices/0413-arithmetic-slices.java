e12176628181a4c1c161ec72961fdd4b9a4124d2
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-07-07 16:34:27 +0530

    Time: 108 ms (5.09%), Space: 41.2 MB (51%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length<3)return 0;
+        int c=0;
+        int a=0;
+        
+        for(int i=0;i<nums.length-2;i++){
+            if((2*nums[i+1])==(nums[i]+nums[i+2])){
+                a++;
+                c+=a;
+            }
+            else a=0;
+            
+        }
+        
+        return c;
+    }
+}
