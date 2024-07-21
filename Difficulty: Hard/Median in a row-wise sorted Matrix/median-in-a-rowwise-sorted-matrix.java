6404d9d431a99bd4502cb1b41c078a1c9e1b1738
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-07-22 00:10:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int median(int matrix[][], int R, int C) {
+        // code here   
+        List<Integer> a=new ArrayList<>();
+        for(int[] i : matrix){
+            for(int j=0;j<C;j++){
+                a.add(i[j]);
+            }
+        }
+        
+        Collections.sort(a);
+        if(a.size()%2==0){
+            return (a.get(a.size()/2-1)+a.get(a.size()/2))/2;
+        }
+        return a.get(a.size()/2);
+    }
+}
