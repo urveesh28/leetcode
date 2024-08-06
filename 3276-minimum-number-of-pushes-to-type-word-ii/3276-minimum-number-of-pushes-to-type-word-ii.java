8b471a40f86c932d38146a2ce202ea9378966998
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-08-06 16:32:31 +0530

    Time: 259 ms (5%), Space: 45.6 MB (64.04%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,46 @@
+class Pair{
+    char ch;
+    int count;
+
+    public Pair(char ch,int count){
+        this.ch=ch;
+        this.count=count;
+    }
+}
+
+class Solution {
+    public int minimumPushes(String word) {
+        
+        Map<Character,Integer> map=new HashMap<>();
+        
+        for(char ch : word.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        
+        int k=0;
+        int[] arr=new int[map.size()];
+
+        for(char i : map.keySet()){
+            arr[k++]=map.get(i);
+        }
+
+        Arrays.sort(arr);
+        int c=0;
+        int count=1;
+        int d=0;
+
+        
+
+        for(int i=arr.length-1;i>=0;i--){
+            if(d%8==0 && d>0){
+                count++;
+            }
+            c+=(arr[i]*count);
+            d++;
+        }
+
+        return c;
+
+    }
+}
