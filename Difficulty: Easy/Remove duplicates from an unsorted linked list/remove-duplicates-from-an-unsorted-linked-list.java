cfb4c8744b0728b1277ff8c0719d9afa641388b4
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2024-11-21 21:09:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.HashSet;
+
+
+// } Driver Code Ends
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+import java.util.*;
+class Solution {
+    // Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) {
+        // Your code here
+        if(head.next==null){
+            return head;
+        }
+        Set<Integer> set=new HashSet<>();
+        Node temp=head;
+        Node prev=null;
+        while(temp!=null){
+            if(set.contains(temp.data)){
+                prev.next=temp.next;
+                
+            }
+            else{
+                set.add(temp.data);
+                prev=temp;
+                
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Driver_code {
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            return temp;
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            Node head = null;
+            String[] str = read.readLine().trim().split(" ");
+            for (String s : str) {
+                head = insert(head, Integer.parseInt(s));
+            }
+            Solution g = new Solution();
+            head = g.removeDuplicates(head);
+            printList(head);
+        }
+    }
+}
+
+// } Driver Code Ends
