eac7bb420468167d7467d6f11ce5649f4e7627cb
Urveesh Mathur <113619189+urveesh28@users.noreply.github.com>
2025-07-20 13:36:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/No Parameter Lambda Expression/no-parameter-lambda-expression.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static Hello helperFunction() {
+        // Your code here
+        
+        return()->System.out.println("Hello");
+        // Implement sayHello using lambda expression and return the object.
+        // Write this in the lambda expression: System.out.println("Hello")
+    }
+}
